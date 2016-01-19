341e50a757aca6361be80de26dbc2662c51b57c4
samueleng <tridentsam@hotmail.com>
2016-01-19 21:50:39 +0800

    Sign Up, Sign In Done

--- TechUna/controllers/signInCtrl.js
+++ TechUna/controllers/signInCtrl.js
@@ -1,27 +1,25 @@
 var app = angular.module("signInApp", ['ngRoute','firebase'])  
 
            
-app.controller('signInCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+app.controller('signInCtrl', ['$scope', '$firebaseAuth',function($scope,$firebaseAuth) {
   var firebaseObj = new Firebase("https://samengfire.firebaseio.com/");
-  var loginObj = $firebaseSimpleLogin(firebaseObj);
+  var loginObj = $firebaseAuth(firebaseObj);
   
-  $scope.user = {};
+  
   $scope.SignIn = function(e){ 
      e.preventDefault();
      var username = $scope.user.email;
      var password = $scope.user.password;
-     loginObj.$login('password', {
-                email: username,
-                password: password
-            })
-            .then(function(user) {
-                //Success callback
-                console.log('Authentication successful'); 
-                alert("Authentication successful");
-            }, function(error) {
-                //Failure callback
-                console.log('Authentication failure'); 
-                alert("Authentication failure");
-            });
+        loginObj.$authWithPassword({
+            email: username,
+            password: password
+        })
+        .then(function(user) {
+            //Success callback
+            console.log('Authentication successful');
+        }, function(error) {
+            //Failure callback
+            console.log('Authentication failure');
+        });
   }
 }]);
---
+++ TechUna/controllers/signUpCtrl.js
@@ -0,0 +1,48 @@
+var app = angular.module("signUpApp", ['ngRoute','firebase'])  
+
+           
+app.controller('signUpCtrl', ['$scope','$window','$firebaseAuth',function($scope, $window, $firebaseAuth) {
+  var firebaseObj = new Firebase("https://samengfire.firebaseio.com/");
+  var auth = $firebaseAuth(firebaseObj) 
+
+  $scope.signUp = function() {  
+    
+        if (!$scope.regForm.$invalid) { 
+            var email = $scope.user.email;
+            var password = $scope.user.password; 
+            var confirmPassword = $scope.user.confirmPassword;
+            if (email && password) {
+                auth.$createUser({email: email, password: password})
+                    .then(function() {
+                        // do things if success 
+                        $window.location.href = '/signin.html'; 
+                        console.log('User creation success');  
+                        
+                    }, function(error) {
+                        // do things if failure
+                        console.log(error);   
+                        $scope.regError = true;
+                        $scope.regErrorMessage = error.message;
+                    });
+            }
+        }
+    };
+}]);  
+   
+angular.module('UserValidation', []).directive('validPasswordC', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            
+              ctrl.$setValidity('noMatch', true);
+
+                attrs.$observe('validPasswordC', function (newVal) {
+                    if (newVal === 'true') {
+                        ctrl.$setValidity('noMatch', true);
+                    } else {
+                        ctrl.$setValidity('noMatch', false);
+                    }
+                });
+        }
+    }
+})
