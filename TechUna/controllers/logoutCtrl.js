5023d97d4426d9992fbc25899a31fec68e36dcba
samueleng <tridentsam@hotmail.com>
2016-01-28 19:11:43 +0800

    Prototype v4 - persistent sign in and signout

---
+++ TechUna/controllers/logoutCtrl.js
@@ -0,0 +1,24 @@
+angular
+	.module(
+		'App'
+	)
+	.controller(
+		'LogoutController',
+		[
+			'$scope',
+			'$rootScope',
+			'$location',
+			'AUTH_EVENTS',
+			'AuthService',
+			function($scope, $rootScope, $location, AUTH_EVENTS, AuthService) {
+				$scope.logout = function() {
+					AuthService.logout();
+
+					$rootScope.$broadcast(AUTH_EVENTS.logout);
+					$scope.setCurrentUser(null);
+
+					$location.path('/home');
+				}
+			}
+		]
+	);
